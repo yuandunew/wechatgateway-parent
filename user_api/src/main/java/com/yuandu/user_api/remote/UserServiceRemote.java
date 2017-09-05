ccf0b03bcb1d8ec46463db0b81480db64e6e8e63
longyuanbing <longyuanbing@lifesense.com>
2017-09-05 09:06:36 +0800

    aaa

---
+++ user_api/src/main/java/com/yuandu/user_api/remote/UserServiceRemote.java
@@ -0,0 +1,14 @@
+package com.yuandu.user_api.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "user")
+public  interface UserServiceRemote {
+
+    @RequestMapping(value = "/group/{groupId}", method = RequestMethod.GET)
+    String findByGroupId(@PathVariable("groupId") Integer adGroupId);
+
+}
