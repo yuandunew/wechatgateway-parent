7ecb7c89b2b36cd69eed1f0b229d0e93cc263eca
longyuanbing <longyuanbing@lifesense.com>
2017-09-12 23:14:10 +0800

    junit test

--- user-api/src/main/java/com/yuandu/user_api/remote/UserServiceRemote.java
+++ user-api/src/main/java/com/yuandu/user_api/remote/UserServiceRemote.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "user")
-public  interface UserServiceRemote {
+public interface UserServiceRemote {
 
     @RequestMapping(value = "/group/{groupId}", method = RequestMethod.GET)
     String findByGroupId(@PathVariable("groupId") Integer adGroupId);
--- user-rest/src/main/java/com/yuandu/App.java
+++ user-rest/src/main/java/com/yuandu/App.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @ComponentScan(basePackages={"com.yuandu"})
 @MapperScan(basePackages="com.yuandu.user.dao.mapper")
 public class App {
