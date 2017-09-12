8d4a3c0f98ec4a285f42ee6e679fc0cba4192c02
longyuanbing <longyuanbing@lifesense.com>
2017-09-12 21:36:28 +0800

    junit test

--- user-service/src/test/java/com/yuandu/user/service/test/TestUser.java
+++ user-service/src/test/java/com/yuandu/user/service/test/TestUser.java
@@ -1,10 +1,10 @@
 package com.yuandu.user.service.test;
 
+import com.yuandu.TestApp;
 import com.yuandu.common.date.DateUtils;
 import com.yuandu.common.exceptions.YuanduBaseException;
 import com.yuandu.common.exceptions.YuanduExceptionCode;
 import com.yuandu.common.json.JsonUtils;
-import com.yuandu.App;
 import com.yuandu.user.dao.model.UserEntity;
 import com.yuandu.redis.repository.RedisObjectRepository;
 import com.yuandu.user.service.UserService;
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
+@SpringApplicationConfiguration(classes = TestApp.class)
 public class TestUser {
 
     @Autowired
@@ -33,7 +33,7 @@ public class TestUser {
     @Test
     public void test() throws Exception {
         UserEntity user = new UserEntity();
-        user.setId(8L);
+        user.setId(9L);
         user.setName("龙");
         user.setSex(1);
         user.setYuanduId("yuandu1");
